fbcf0f5598492dc2596ec57eaf2c20940f8f36b8
bharadiya <shashank.bharadiya@gmail.com>
2019-07-03 12:21:13 +0530

    small changes

---
+++ src/DescendingSorting.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class DescendingSorting {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		int temp = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] < arr[j + 1]) {
+					temp = arr[j + 1];
+					arr[j + 1] = arr[j];
+					arr[j] = temp;
+				} else {
+					continue;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
